21369511610636b6c5b416fc941b4cd451098eb0
Pietari Järvi <pietari.jarvi@gmail.com>
2020-03-31 00:33:59 +0300

    Create Main.java

---
+++ ObserverProject/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {	
+	@SuppressWarnings("unused")
+	public static void main(String[] args){
+		DigitalClock dig = new DigitalClock(Clock.getInstance());		Thread clockThread = new Thread(Clock.getInstance());		clockThread.run();
+	}
+}
